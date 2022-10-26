ae40d115537ca8c231f5f685fba982853374133a
dusanlekic22 <lekic.ra159.2018@uns.ac.rs>
2022-10-26 16:30:13 +0200

    feat: user repository

---
+++ tests-backend/sotis-project/src/main/java/com/example/sotisproject/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.sotisproject.repository;
+
+import com.example.sotisproject.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository  extends JpaRepository<User, Long> {
+        User findByUsername(String username);
+        User findByEmail(String userEmail);
+}
