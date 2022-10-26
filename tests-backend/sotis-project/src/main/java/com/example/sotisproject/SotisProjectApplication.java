512a5a209849410dd5c75c70609e2b537c568df4
dusanlekic22 <lekic.ra159.2018@uns.ac.rs>
2022-10-26 14:17:29 +0200

    feat: initialized backend

---
+++ tests-backend/sotis-project/src/main/java/com/example/sotisproject/SotisProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.sotisproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SotisProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SotisProjectApplication.class, args);
+	}
+
+}
---
+++ tests-backend/sotis-project/src/test/java/com/example/sotisproject/SotisProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sotisproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SotisProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
