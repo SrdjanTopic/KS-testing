886aee9b6d1dd96607b5bd0cd373014e940e1f03
SrdjanTopic <64950418+SrdjanTopic@users.noreply.github.com>
2022-12-01 16:02:11 +0100

    question order for tests working

--- tests-backend/sotis-project/src/main/java/com/example/sotisproject/service/RelationService.java
+++ tests-backend/sotis-project/src/main/java/com/example/sotisproject/service/RelationService.java
@@ -24,9 +24,37 @@ public class RelationService {
     }
 
     public List<Long> getConceptOrder(){
+        List<Long> sources = new ArrayList<>();
+        List<Long> destinations = new ArrayList<>();
         List<Long> conceptOrder = new ArrayList<>();
+        List<Long> currentConcepts = new ArrayList<>();
+        List<Long> tempCurr = new ArrayList<>();
         List<Relation> relationList = relationRepository.findAll();
-
+        relationList.forEach(relation -> {
+            sources.add(relation.getSource().getId());
+            destinations.add(relation.getDestination().getId());
+        });
+        sources.forEach(source->{if(!destinations.contains(source)&&!conceptOrder.contains(source)&&!currentConcepts.contains(source)) {
+            conceptOrder.add(source);
+            currentConcepts.add(source);
+        }});
+        sources.removeAll(currentConcepts);
+        do{
+            relationList.forEach(relation -> {
+                if (currentConcepts.contains(relation.getSource().getId())&&!conceptOrder.contains(relation.getDestination().getId())) {
+                    conceptOrder.add(relation.getDestination().getId());
+                    tempCurr.add(relation.getDestination().getId());
+                }
+            });
+            currentConcepts.clear();
+            currentConcepts.addAll(tempCurr);
+            sources.removeAll(currentConcepts);
+        }while(sources.size()!=0);
+        relationList.forEach(relation -> {
+            if (currentConcepts.contains(relation.getSource().getId())&&!conceptOrder.contains(relation.getDestination().getId())) {
+                conceptOrder.add(relation.getDestination().getId());
+            }
+        });
         return conceptOrder;
     }
 }
