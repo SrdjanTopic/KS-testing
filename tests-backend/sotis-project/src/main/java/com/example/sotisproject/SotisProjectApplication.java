2a161b91c896a9e6eb3c31f52e93d80d1e02423a
SrdjanTopic <64950418+SrdjanTopic@users.noreply.github.com>
2022-12-12 15:10:51 +0100

    added Jena dependency for Semantic Web

--- tests-backend/sotis-project/src/main/java/com/example/sotisproject/SotisProjectApplication.java
+++ tests-backend/sotis-project/src/main/java/com/example/sotisproject/SotisProjectApplication.java
@@ -1,5 +1,9 @@
 package com.example.sotisproject;
 
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.vocabulary.VCARD;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +12,11 @@ public class SotisProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SotisProjectApplication.class, args);
+		Model m = ModelFactory.createDefaultModel();
+		Resource resource1 = m.createResource("http://rdfData/resource1");
+		resource1.addProperty(VCARD.FN,  "Some kind of resource");
+
+		m.write(System.out);
 	}
 
 }
